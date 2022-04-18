229b9dfbc727fb7be27bba69fe28dc7e7f0905e0
Peter Abrahamsen <rainhead@gmail.com>
2022-04-17 17:46:06 -0700

    Enable whitenoise

--- pedalingrelief/settings.py
+++ pedalingrelief/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pedalingrelief.urls'
