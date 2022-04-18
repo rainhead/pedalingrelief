7494227f64ab656505d0e38b9ea88f4e53d9c166
Peter Abrahamsen <rainhead@gmail.com>
2022-04-17 20:08:15 -0700

    Admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.Feature)
+admin.site.register(models.FoodBank)
+admin.site.register(models.Item)
+admin.site.register(models.Pantry)
+admin.site.register(models.Person)
+admin.site.register(models.Restock)
+admin.site.register(models.DeliveryRoute)
