66b39ed9fb2949ac66807bff40b61949b1dc70fa
Peter Abrahamsen <rainhead@gmail.com>
2022-04-18 17:32:38 -0700

    Update database backend

--- pedalingrelief/settings.py
+++ pedalingrelief/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'pedalingrelief.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'psqlextra.backend',
         'NAME': 'pedalingrelief',
     }
 }
